54e635d359c699fdf385b8781b80b65c1edb8ff3
Zhou <liang.zhou@miun.se>
2023-05-15 13:23:55 +0200

    made it show selected min and max value text above the slide thumb.
    Signed-off-by: Zhou <liang.zhou@miun.se>

--- src/components/FilterCanvas.js
+++ src/components/FilterCanvas.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import Select from 'react-select';
-import Slider from 'rc-slider';
+//import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
-
+import { Slider, Rail, Handles, Tracks, Ticks } from 'react-compound-slider';
 
 
 const FilterRow = ({ fields, onAdd, onRemove, handleRangeChange, rangeValues }) => {
@@ -32,6 +32,62 @@ const FilterRow = ({ fields, onAdd, onRemove, handleRangeChange, rangeValues }) 
     width: '400px', // Set the desired length of the slider here
   };
 
+
+
+  // Define a custom handle component to display the selected min and max values
+  // const Handle = ({ handle: { id, value, percent }, getHandleProps }) => {
+    
+  //   return (
+  //     <div
+  //       style={{
+  //         left: `${percent}%`,
+  //         position: 'absolute',
+  //         marginLeft: -15,
+  //         marginTop: 25,
+  //         zIndex: 2,
+  //         width: 30,
+  //         height: 30,
+  //         border: 0,
+  //         textAlign: 'center',
+  //         cursor: 'pointer',
+  //         borderRadius: '50%',
+  //         backgroundColor: '#2C4870',
+  //         color: '#333',
+  //       }}
+  //       {...getHandleProps(id)}
+  //     >
+  //       <div style={{ fontFamily: 'Arial', fontSize: 'smaller', marginTop: -35 }}>{value}</div>
+  //     </div>
+  //   )
+  // }
+  
+  const Handle = ({ handle: { id, value, percent }, getHandleProps }) => {
+    return (
+      <div
+        style={{
+          left: `${percent}%`,
+          position: 'absolute',
+          marginLeft: -10, 
+          marginTop: 0, // adjust the position to make the handle on the track
+          zIndex: 2,
+          width: 15, // make the handle smaller
+          height: 15, // make the handle smaller
+          border: 0,
+          textAlign: 'center',
+          cursor: 'pointer',
+          borderRadius: '50%',
+          backgroundColor: '#2C4870',
+          color: '#333',
+        }}
+        {...getHandleProps(id)}
+      >
+        <div style={{ fontFamily: 'Arial', fontSize: 'smaller', marginTop: -25 }}>{value}</div> 
+      </div>
+    )
+  }
+  
+
+
   return (
     <div className="filter-row" style={selectorContainerStyle}>
       <Select
@@ -50,7 +106,75 @@ const FilterRow = ({ fields, onAdd, onRemove, handleRangeChange, rangeValues }) 
         styles={selectStyles}
         placeholder="Subject...d"
       />
-      <div style={sliderContainerStyle} >
+
+      
+      <div style={sliderContainerStyle}>
+        <Slider
+          rootStyle={{ position: 'relative', width: '100%' }}
+          domain={rangeValues || [0, 100]}
+          step={1}
+          mode={2}
+          values={range}
+          onChange={handleSliderChange}
+        >
+          <Rail>
+            {({ getRailProps }) => (
+              <div style={{ position: 'absolute', width: '100%', height: 14, borderRadius: 7, cursor: 'pointer', backgroundColor: 'rgb(155,155,155)' }} {...getRailProps()} />
+            )}
+          </Rail>
+          <Handles>
+            {({ handles, getHandleProps }) => (
+              <div className="slider-handles">
+                {handles.map(handle => (
+                  <Handle
+                    key={handle.id}
+                    handle={handle}
+                    getHandleProps={getHandleProps}
+                  />
+                ))}
+              </div>
+            )}
+          </Handles>
+          <Tracks left={false} right={false}>
+            {({ tracks, getTrackProps }) => (
+              <div className="slider-tracks">
+                {tracks.map(({ id, source, target }) => (
+                  <div
+                    key={id}
+                    className="slider-track"
+                    style={{
+                      position: 'absolute',
+                      height: 14,
+                      zIndex: 1,
+                      backgroundColor: '#546C91',
+                      borderRadius: 7,
+                      cursor: 'pointer',
+                      left: `${source.percent}%`,
+                      width: `${target.percent - source.percent}%`,
+                    }}
+                    {...getTrackProps()}
+                  />
+                ))}
+              </div>
+            )}
+          </Tracks>
+          <Ticks count={10}>
+            {({ ticks }) => (
+              <div className="slider-ticks">
+              {ticks.map(tick => (
+                  <div key={tick.id} style={{ position: 'absolute', marginTop: 14, marginLeft: -0.5, width: 1, height: 5, backgroundColor: 'rgb(200,200,200)', left: `${tick.percent}%` }} />
+                ))}
+              </div>
+            )}
+          </Ticks>
+        </Slider>
+      </div>
+
+      {/* <div style={sliderContainerStyle}>
+        <div>
+          <span>{rangeValues ? rangeValues[0] : 0}</span> 
+          <span style={{ float: 'right' }}>{rangeValues ? rangeValues[1] : 100}</span>
+        </div>
         <Slider
           range
           min={rangeValues ? rangeValues[0] : 0}
@@ -59,7 +183,12 @@ const FilterRow = ({ fields, onAdd, onRemove, handleRangeChange, rangeValues }) 
           onChange={handleSliderChange}
           styles={selectStyles}
         />
-      </div>
+        <div>
+          <span>{range[0]}</span> 
+          <span style={{ float: 'right' }}>{range[1]}</span>
+        </div>
+      </div> */}
+
       <button onClick={onAdd}>Add</button>
       <button onClick={onRemove}>Remove</button>
     </div>
