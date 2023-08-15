7fd7220fc9750adee41f729781e6aa45b78704c5
Zhou <liang.zhou@miun.se>
2023-08-15 14:41:04 +0200

    Filtered unnecessary entries from the detail canvas.
    Signed-off-by: Zhou <liang.zhou@miun.se>

--- src/components/DetailCanvas.js
+++ src/components/DetailCanvas.js
@@ -1,10 +1,21 @@
 import React from 'react';
 
 const DetailCanvas = ({ data }) => {
-  console.log("data type: "+ typeof(data) + " content:" + data );
+  //console.log("data type: "+ typeof(data) + " content:" + data );
+  const keyList = 
+  ['Skola',
+  'Årskurs',
+  'Klass',
+  'ElevID',
+  'Födelsedatum',
+  'Läsår',
+  'Testdatum',
+  'Standardpoäng',
+  'Lexplore Score'
+  ];
   return (
     <div className="detail-canvas">
-      {data && Object.entries(data).map(([key, value]) => (
+      {data && Object.entries(data).filter(([key]) => keyList.includes(key)).map(([key, value]) => (
         <div key={key}>
           <strong>{key}:</strong> {value}
         </div>
