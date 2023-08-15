cb7f7c58129ea0458835e6ceb437f9c18dbd8029
Zhou <liang.zhou@miun.se>
2023-08-15 14:28:16 +0200

    Non-UTF8 formatted CSV replaced by UTF8 CSV. Changed display of buttons.
    Signed-off-by: Zhou <liang.zhou@miun.se>

--- src/components/AxisSelectionCanvas.js
+++ src/components/AxisSelectionCanvas.js
@@ -11,50 +11,64 @@ const AxisSelectionCanvas = ({ fields, xField, yField, colorField, onXFieldChang
   
   return (
     <div className="axis-selection-canvas">
-      <label htmlFor="x-field">X-field:</label>
-      <Select
-        id="x-field"
-        value={{ value: xField, label: xField }}
-        options={options}
-        onChange={option => onXFieldChange(option.value)}
-      />
-      <label htmlFor="y-field">Y-field:</label>
-      <Select
-        id="y-field"
-        value={{ value: yField, label: yField }}
-        options={options}
-        onChange={option => onYFieldChange(option.value)}
-      />
-      <label htmlFor="Colorfiled">Color:</label>
-      <Select
-        id="color-field"
-        value={{ value: colorField, label: colorField }}
-        options={options}
-        onChange={option => onColorFieldChange(option.value)}
-      />
 
-      <button 
-          id="switch-class-student-view-btn"
-          onClick={() => onSwitchView()} // function to switch between class or student view.
+      <div className="axis-selects-row"  style={{ display: 'flex' }} >
+        <div className="field-pair">
+          <label htmlFor="x-field">X-field:</label>
+          <Select
+            id="x-field"
+            value={{ value: xField, label: xField }}
+            options={options}
+            onChange={option => onXFieldChange(option.value)}
+          />
+        </div>
+
+        <div className="field-pair">
+          <label htmlFor="y-field">Y-field:</label>
+          <Select
+            id="y-field"
+            value={{ value: yField, label: yField }}
+            options={options}
+            onChange={option => onYFieldChange(option.value)}
+          />
+        </div>
+
+        <div className="field-pair">
+          <label htmlFor="Colorfiled">Color:</label>      
+          <Select
+            id="color-field"
+            value={{ value: colorField, label: colorField }}
+            options={options}
+            onChange={option => onColorFieldChange(option.value)}
+          />
+        </div>
+      </div>
+
+      <div className="preset-buttons-row" >
+        <button 
+            id="switch-class-student-view-btn"
+            onClick={() => onSwitchView()} // function to switch between class or student view.
+          >
+            {isClassView ?  'Student View': 'Class View' }
+          </button>
+
+        <button 
+            id="save-preset-btn"
+            //style={{ color: 'gray' }} 
+            onClick={() => onSavePreset()} // function to save current state as a preset
+          >
+            Save Preset
+          </button>
+
+        <button 
+          id="load-preset-btn"
+          onClick={() => onLoadPreset()} // function to load a saved preset
         >
-          {isClassView ?  'Student View': 'Class View' }
+          Load Preset
         </button>
-
-      <button 
-          id="save-preset-btn"
-          //style={{ color: 'gray' }} 
-          onClick={() => onSavePreset()} // function to save current state as a preset
-        >
-          Save Preset As
-        </button>
-
-      <button 
-        id="load-preset-btn"
-        onClick={() => onLoadPreset()} // function to load a saved preset
-      >
-        Load Preset
-      </button>
+      </div>
     </div>
+  
   );
 };
 
