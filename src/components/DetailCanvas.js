8e67d2367bff81820dfbafce94a42ee5d147f95b
Zhou <liang.zhou@miun.se>
2023-10-05 13:17:45 +0200

    Make two-column layout for detail canvas.
    Signed-off-by: Zhou <liang.zhou@miun.se>

--- src/components/DetailCanvas.js
+++ src/components/DetailCanvas.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import * as d3 from 'd3';
+import '../App.css';
 
 const DetailCanvas = ({ data }) => {
   //console.log("data type: "+ typeof(data) + " content:" + data );
@@ -17,7 +18,7 @@ const DetailCanvas = ({ data }) => {
   return (
     <div className="detail-canvas">
       {data && Object.entries(data).filter(([key]) => keyList.includes(key)).map(([key, value]) => (
-        <div key={key}>
+        <div key={key} className="detail-item">
           <strong>{key}:</strong> {DetailValue(key,value)}
         </div>
       ))}
