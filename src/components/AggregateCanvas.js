a4e124d379de702c95f61b997cbb4dd3232a7211
Liang Zhou <liang.zhou@miun.se>
2023-12-08 13:46:00 +0100

    solve Order of hooks problems for violin plots by adding  newXScaleRef, newYScaleRef.
    Signed-off-by: Liang Zhou <liang.zhou@miun.se>

--- src/components/AggregateCanvas.js
+++ src/components/AggregateCanvas.js
@@ -21,84 +21,86 @@ const AggregateCanvas = ({ data, filteredData, xField, yField, colorField, width
 
 
 const ViolinPlots = (filteredData, xField, yField, colorField, width, height,  onViolinClick, selectedRecord, studentsChecked ) => {
-        const svgRef = useRef();
-        const parseDate = d3.timeParse('%y%m%d');
-        const formatDate = d3.timeFormat('%y-%m-%d');
+    const svgRef = useRef();
+    const parseDate = d3.timeParse('%y%m%d');
+    const formatDate = d3.timeFormat('%y-%m-%d');
+    const newXScaleRef = useRef(null);
+    const newYScaleRef = useRef(null);
 
-        useEffect(() => {
+    useEffect(() => {
 
-            const {svg, g, margin, innerWidth, innerHeight, colorScale, sumstat, y, x0, xAxis, getSubBandScale, lastingClassGroups, identityClasses}  = PreparePlotStructure(svgRef, filteredData, yField, width, height, true);
+        const {svg, g, margin, innerWidth, innerHeight, colorScale, sumstat, y, x0, xAxis, getSubBandScale, lastingClassGroups, identityClasses}  = PreparePlotStructure(svgRef, filteredData, yField, width, height, true);
 
-            // For the X axis label:
-            g.append("text")
-            .attr("y", innerHeight + margin.bottom / 2)
-            .attr("x", innerWidth / 2)  
-            .attr("dy", "1em")    
-            .style("text-anchor", "middle")  
-            .text("Seasons of Testdatum"); 
+        // For the X axis label:
+        g.append("text")
+        .attr("y", innerHeight + margin.bottom / 2)
+        .attr("x", innerWidth / 2)  
+        .attr("dy", "1em")    
+        .style("text-anchor", "middle")  
+        .text("Seasons of Testdatum"); 
 
-            // Add the x-axis to the group element
-            g.append("g")
-                .attr("transform", `translate(0, ${innerHeight})`)
-                .call(xAxis);        
+        // Add the x-axis to the group element
+        g.append("g")
+            .attr("transform", `translate(0, ${innerHeight})`)
+            .call(xAxis);        
 
 
-            g.append('g').call(d3.axisLeft(y).tickFormat(d => {
-                if(yField==='Födelsedatum'||yField==='Testdatum')
-                {const dateObject = parseDate(d);
-                return formatDate(dateObject);
-                }
-                return d;
-            }));
-
-            g.append("text")
-            .attr("transform", "rotate(-90)")
-            .attr("y", -margin.left)
-            .attr("x", -innerHeight / 2) 
-            .attr("dy", "1em")  
-            .style("text-anchor", "middle")
-            .text(yField); 
-
-            
-            const subBandWidth = x0.bandwidth() * 0.1;
-
-            var maxNum = 0;
-            for (var i in sumstat) {
-                var allBins = sumstat[i].value.bins
-                var lengths = allBins.map(a => a.length)
-                var longest = d3.max(lengths)
-                if (longest > maxNum) { maxNum = longest }
+        g.append('g').call(d3.axisLeft(y).tickFormat(d => {
+            if(yField==='Födelsedatum'||yField==='Testdatum')
+            {const dateObject = parseDate(d);
+            return formatDate(dateObject);
             }
+            return d;
+        }));
 
-            const xNum = d3.scaleLinear().range([0, subBandWidth]).domain([-maxNum, maxNum]);
+        g.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", -margin.left)
+        .attr("x", -innerHeight / 2) 
+        .attr("dy", "1em")  
+        .style("text-anchor", "middle")
+        .text(yField); 
 
-            const widthNum = 2;
+        
+        const subBandWidth = x0.bandwidth() * 0.1;
+
+        var maxNum = 0;
+        for (var i in sumstat) {
+            var allBins = sumstat[i].value.bins
+            var lengths = allBins.map(a => a.length)
+            var longest = d3.max(lengths)
+            if (longest > maxNum) { maxNum = longest }
+        }
+
+        const xNum = d3.scaleLinear().range([0, subBandWidth]).domain([-maxNum, maxNum]);
+
+        const widthNum = 2;
             g.selectAll("myViolin")
                 .data(sumstat)
                 .enter().append("g")
                 .attr("transform",  d => {
-                    const season = d.value.season;
-                    const clazz = d.value.class;
-                    const x1 = getSubBandScale(season); // Get x1 scale for the current season
-                    return `translate(${x0(season) + x1(clazz)}, 0)`;
-                })
-                .style("fill", d => {
-                    const classId = getLastingClassID(d.value.school, d.value.season, d.value.class);
-                    return colorScale(classId);
-                })
+                const season = d.value.season;
+                const clazz = d.value.class;
+                const x1 = getSubBandScale(season); // Get x1 scale for the current season
+                return `translate(${x0(season) + x1(clazz)}, 0)`;
+            })
+            .style("fill", d => {
+                const classId = getLastingClassID(d.value.school, d.value.season, d.value.class);
+                return colorScale(classId);
+            })
                 .on("click", (event, d) => {
-                    onViolinClick([{
-                        lastingclass: d.value.lastingclass,
-                        class: d.value.class,
-                        season: d.value.season,
-                        min: parseInt(d.value.min)  , 
-                        max: parseInt(d.value.max)   , 
-                        median: parseInt(d.value.median) , 
-                        q1: parseInt(d.value.q1) ,
-                        q3: parseInt(d.value.q3)  ,
-                        count: parseInt(d.value.count) 
-                    }]);
-                })
+                onViolinClick([{
+                    lastingclass: d.value.lastingclass,
+                    class: d.value.class,
+                    season: d.value.season,
+                    min: parseInt(d.value.min)  , 
+                    max: parseInt(d.value.max)   , 
+                    median: parseInt(d.value.median) , 
+                    q1: parseInt(d.value.q1) ,
+                    q3: parseInt(d.value.q3)  ,
+                    count: parseInt(d.value.count) 
+                }]);
+            })
                 .append("path")
                 .datum(
                     function(d){ 
@@ -107,56 +109,56 @@ const ViolinPlots = (filteredData, xField, yField, colorField, width, height,  o
                 )
                 .style("stroke", "none")
                 .attr("d", d3.area()
-                    .x0(d => xNum(-d.length* widthNum))  
-                    .x1(d => xNum(d.length* widthNum))  
-                    .y(d => y(d.x0))   //d.x0
-                    .curve(d3.curveCatmullRom)
+                .x0(d => xNum(-d.length* widthNum))  
+                .x1(d => xNum(d.length* widthNum))  
+                .y(d => y(d.x0))   //d.x0
+                .curve(d3.curveCatmullRom)
                 )
 ;  
 
 
-            lastingClassGroups.forEach((values, lastingClassKey) => {
-                for(let i = 0; i < values.length - 1; i++) {
-                    const startPoint = values[i];
-                    const endPoint = values[i + 1]; 
-                    g.append("line")
-                        .attr("x1", () => {
-                            const season = startPoint.value.season.toString();
-                            const clazz = startPoint.value.class.toString();
-                            const x1 = getSubBandScale(season);
-                            return x0(season) + x1(clazz) + subBandWidth / 2;
-                        })
-                        .attr("x2", () => {
-                            const season = endPoint.value.season.toString();
-                            const clazz = endPoint.value.class.toString();
-                            const x1 = getSubBandScale(season);
-                            return x0(season) + x1(clazz) + subBandWidth / 2;
-                        })
-                        .attr("y1", y(startPoint.value.median))
-                        .attr("y2", y(endPoint.value.median))
-                        .attr("stroke", d => {            
-                            const classId = getLastingClassID(startPoint.value.school, startPoint.value.season, startPoint.value.class);
-                            return colorScale(classId);}) 
-                        .attr('stroke-width', 1.5); 
-                }
-            }); 
+        lastingClassGroups.forEach((values, lastingClassKey) => {
+            for(let i = 0; i < values.length - 1; i++) {
+                const startPoint = values[i];
+                const endPoint = values[i + 1]; 
+                g.append("line")
+                    .attr("x1", () => {
+                        const season = startPoint.value.season.toString();
+                        const clazz = startPoint.value.class.toString();
+                        const x1 = getSubBandScale(season);
+                        return x0(season) + x1(clazz) + subBandWidth / 2;
+                    })
+                    .attr("x2", () => {
+                        const season = endPoint.value.season.toString();
+                        const clazz = endPoint.value.class.toString();
+                        const x1 = getSubBandScale(season);
+                        return x0(season) + x1(clazz) + subBandWidth / 2;
+                    })
+                    .attr("y1", y(startPoint.value.median))
+                    .attr("y2", y(endPoint.value.median))
+                    .attr("stroke", d => {            
+                        const classId = getLastingClassID(startPoint.value.school, startPoint.value.season, startPoint.value.class);
+                        return colorScale(classId);}) 
+                    .attr('stroke-width', 1.5); 
+            }
+        }); 
 
 
             // Add individual points with jitter
-  
-            if(studentsChecked) {       
-                PresentIndividuals(filteredData, yField, g, x0, getSubBandScale, y, subBandWidth)       
-                
-            }
 
-            // Add color legend
-            ColorLegend(identityClasses, "classID", svg, 200, margin);   
+        if(studentsChecked) {       
+            PresentIndividuals(filteredData, yField, g, x0, getSubBandScale, y, subBandWidth)       
+            
+        }
 
-        }, [filteredData,xField, yField, colorField, width, height, selectedRecord, studentsChecked, formatDate, parseDate, onViolinClick]);
-        
-        return (
-            <svg className="scatter-canvas" ref={svgRef} width={width} height={height}></svg>
-        );
+        // Add color legend
+        ColorLegend(identityClasses, "classID", svg, 200, margin);   
+
+    }, [filteredData,xField, yField, colorField, width, height, selectedRecord, studentsChecked, formatDate, parseDate, onViolinClick]);
+    
+    return (
+        <svg className="scatter-canvas" ref={svgRef} width={width} height={height}></svg>
+    );
     };
 
 
