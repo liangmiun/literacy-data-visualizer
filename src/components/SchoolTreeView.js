f485f678255e4f672aa4c68c3873c6b8580d9e0c
Liang Zhou <liang.zhou@miun.se>
2024-02-13 16:40:02 +0100

    add indeterminate option for class level in schoo class view.
    Signed-off-by: Liang Zhou <liang.zhou@miun.se>

--- src/components/SchoolTreeView.js
+++ src/components/SchoolTreeView.js
@@ -35,11 +35,14 @@ function SchoolTreeView(props) {
   }
 
   const handleSchoolCheckChange = (school, isChecked) => {
-      if (isChecked) {
+    console.log('school check  ', school, isChecked);
+
+    if (isChecked) {
           props.setCheckedSchools(prev => [...prev, school]);
           props.setCheckedClasses(prev => [...prev, ...Object.keys(props.school_class[school]).map(classId => `${school}.${classId}`)]);
       } else {   
           props.setCheckedSchools(prev => prev.filter(s => s !== school));
+          props.setCheckedClasses(prev => prev.filter(c => !c.startsWith(`${school}.`)));
       }
   };
 
@@ -98,71 +101,28 @@ function SchoolTreeView(props) {
             }  
           
           >
-             {Object.entries(props.school_class).map(([school, classesMap], idx) => (
-              <div key={school} style={{ display: 'flex', alignItems: 'flex-start' }}>
-                <Checkbox  style={{ padding: '1px' }}
-                      checked={props.checkedSchools.includes(school)}
-                      onChange={(event) => {handleSchoolCheckChange(school, event.target.checked)}}
-                />
-                <TreeItem
-                  nodeId={`school-${idx}`}
-                  label={
-                    <div style={{ display: 'flex', alignItems: 'center' }}>
-                      {school }
-                    </div>
-                  }
-                  key={school}
-                >
-                  {Object.entries(classesMap).map(([classId, classes], cIdx) => (
-                    <div key={classId} style={{ display: 'flex', alignItems: 'center', flexDirection: 'row' }}>
-                    <TreeItem
-                      nodeId={`class-${idx}-${cIdx}`}
-                      label={
-                        <div style={{ display: 'flex', alignItems: 'center' }}>
-                          <Checkbox 
-                            checked={props.checkedClasses.includes(`${school}.${classId}`)}
-                            onChange={(event) => handleClassCheckChange(`${school}.${classId}`, event.target.checked)}
-                          />
-                          {classId }
-                        </div>
-                      }
-                      key={classId}
-                    />
-                    {/* <div style={{ width: '10px', height: '10px', backgroundColor: classColors[school](classId), marginLeft: '5px' }} /> */}
-                    <div>
-                      <div 
-                        style={{ width: '10px', height: '10px', backgroundColor: props.isClassView? props.classColors[school][classId]: 'white', marginLeft: '5px' }}
-                        onClick={() => setPaletteID(classId)}
-                      />
-                      {paletteID===classId && props.isClassView && (
-                        <div style={{ marginTop: '5px' }}>
-                          {[0, 1].map(row => (
-                            <div key={row} style={{ display: 'flex' }}>
-                              {d3.schemeCategory10.slice(row * 5, (row + 1) * 5).map((paletteColor, index) => (
-                                <div 
-                                  key={index}
-                                  style={{ width: '10px', height: '10px', backgroundColor:  paletteColor, marginLeft: '2px' }}
-                                  onClick={() => handleColorChange(school, classId, paletteColor)}
-                                />
-                              ))}
-                            </div>
-                          ))}
-                        </div>
-                      )}
-                    </div>
-                  </div>
-                  ))}
-                </TreeItem>
-
-
-                <Button size="small"
-                        onClick={() => {handleSchoolCheckChange(school, false);props.setCheckedClasses(prev => prev.filter(c => !c.startsWith(`${school}.`)))}}
-                        style={{marginLeft: '10px',  minWidth: 0 }}
-                      >
-                          X
-                </Button>
-
-              </div>
+            {Object.entries(props.school_class).map( ([school, classesMap], idx) =>( 
+            
+              <SchoolComponent
+                key={school}
+                props={{
+                  school,
+                  classesMap,
+                  idx,
+                  checkedClasses: props.checkedClasses,
+                  setCheckedClasses: props.setCheckedClasses,
+                  handleClassCheckChange,
+                  checkedSchools: props.checkedSchools,
+                  onSchoolCheckChange: handleSchoolCheckChange,
+                  setPaletteID,
+                  paletteID,
+                  handleColorChange,
+                  isClassView: props.isClassView,
+                  classColors: props.classColors
+                }}
+              />
+            
+            
             ))}
           </TreeItem>
 
@@ -174,13 +134,94 @@ function SchoolTreeView(props) {
   
       </div>
 
-
-
-
-
-
   );
 }
+
+
+function SchoolComponent({ props }) {
+  const { school, classesMap, checkedClasses, onSchoolCheckChange, idx, handleClassCheckChange, 
+        setPaletteID, paletteID, handleColorChange} = props;
+  const classCheckStates = Object.entries(classesMap).map(([classId]) =>
+    checkedClasses.includes(`${school}.${classId}`)
+  );
+
+  const someChecked = classCheckStates.some(checked => checked);
+  const someUnchecked = classCheckStates.some(checked => !checked);
+  const allChecked = classCheckStates.every(checked => checked);
+
+  useEffect(() => {
+    // Trigger the handler when allChecked changes from false to true
+    if (allChecked && !props.checkedSchools.includes(school)) {
+      console.log('all checked');
+      onSchoolCheckChange(school, true);
+    }
+  }, [allChecked, school, onSchoolCheckChange]);
+
+  return (
+    <div style={{ display: 'flex', alignItems: 'flex-start' }}>
+      <Checkbox
+        style={{ padding: '1px' }}
+        checked={props.checkedSchools.includes(school)}
+        indeterminate={someChecked && someUnchecked}
+        onChange={(event) => onSchoolCheckChange(school, event.target.checked)}
+      />
+      {/* Render classes and other UI elements here */}
+
+      <TreeItem
+          nodeId={`school-${idx}`}
+          label={
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+              {school }
+            </div>
+          }
+          key={school}
+        >
+          {Object.entries(classesMap).map(([classId, classes], cIdx) => (
+            <div key={classId} style={{ display: 'flex', alignItems: 'center', flexDirection: 'row' }}>
+            <TreeItem
+              nodeId={`class-${idx}-${cIdx}`}
+              label={
+                <div style={{ display: 'flex', alignItems: 'center' }}>
+                  <Checkbox 
+                    checked={props.checkedClasses.includes(`${school}.${classId}`)}
+                    onChange={(event) => handleClassCheckChange(`${school}.${classId}`, event.target.checked)}
+                  />
+                  {classId }
+                </div>
+              }
+              key={classId}
+            />
+            {/* <div style={{ width: '10px', height: '10px', backgroundColor: classColors[school](classId), marginLeft: '5px' }} /> */}
+            <div>
+              <div 
+                style={{ width: '10px', height: '10px', backgroundColor: props.isClassView? props.classColors[school][classId]: 'white', marginLeft: '5px' }}
+                onClick={() => setPaletteID(classId)}
+              />
+              {paletteID===classId && props.isClassView && (
+                <div style={{ marginTop: '5px' }}>
+                  {[0, 1].map(row => (
+                    <div key={row} style={{ display: 'flex' }}>
+                      {d3.schemeCategory10.slice(row * 5, (row + 1) * 5).map((paletteColor, index) => (
+                        <div 
+                          key={index}
+                          style={{ width: '10px', height: '10px', backgroundColor:  paletteColor, marginLeft: '2px' }}
+                          onClick={() => handleColorChange(school, classId, paletteColor)}
+                        />
+                      ))}
+                    </div>
+                  ))}
+                </div>
+              )}
+            </div>
+          </div>
+          ))}
+      </TreeItem>  
+
+
+    </div>
+  );
+}
+
   
 export default SchoolTreeView;
 
