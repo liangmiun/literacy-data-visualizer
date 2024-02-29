b47878abceb1d1b5a4358835e228176d1aaa182a
Liang Zhou <liang.zhou@miun.se>
2024-02-29 11:58:33 +0100

    Refactor: Make AxisSelectionCanvas components into separate functions.
    Signed-off-by: Liang Zhou <liang.zhou@miun.se>

--- src/components/layouts/AxisSelectionCanvas.js
+++ src/components/layouts/AxisSelectionCanvas.js
@@ -33,7 +33,6 @@ const AxisSelectionCanvas = (props) => {
     props.onTrendChange(event.target.value);
   };
 
-
   // Define the trend-to-string mapping dictionary
   const trendToLabel = {
     [props.trendSet.all]: "  ", // Assuming props.trendSet.all is a constant value
@@ -45,199 +44,240 @@ const AxisSelectionCanvas = (props) => {
   
   return (
     <div className="axis-selection-canvas" >
-      <div className="axis-selects-row"  style={{ display: 'flex' }} >
-        <div className="field-pair"  >
-          <Tooltip title="Select variable on horizontal axis" followCursor>
-            <label htmlFor="x-field">X-field:</label>
-          </Tooltip>
-          <Select 
-            isDisabled = {props.isClassView} 
-            id="x-field"
-            value={{ value: props.xField, label: props.xField }}
-            options={x_options}
-            onChange={option => props.onXFieldChange(option.value)}
-          />
-        </div>       
+      <div className="axis-canvas-row"  style={{ display: 'flex' }} >
+    
+        <Axes props={props} x_options={x_options} y_options={y_options} colorOptions={colorOptions} />
 
-        <div className="field-pair" >
-          <Tooltip title="Select variable on vertical axis" followCursor>
-            <label htmlFor="y-field">Y-field:</label>
-          </Tooltip>
-          <Select
-            id="y-field"
-            value={{ value: props.yField, label: props.yField }}
-            options={y_options}
-            onChange={option => props.onYFieldChange(option.value)}
-          />
-        </div>
+        <TrendBar props={props} trendOptions={trendOptions} handleTrendChange={handleTrendChange} trendToLabel={trendToLabel} />
 
-        <div className="field-pair">
-        <Tooltip title="Select variable for color coded data" followCursor>
-          <label htmlFor="color-field">Color:</label>   
-        </Tooltip>   
-          <Select
-            isDisabled = {props.isClassView}
-            id="color-field"
-            value={{ value: props.colorField, label: props.colorField }}
-            options={colorOptions}
-            onChange={option => props.onColorFieldChange(option.value)}
-          />
-        </div>
+        <ShowLinesToggle props={props} />
 
+        <ClassViewBar props={props} />
 
+        <PresetBar props={props} onSavePreset={onSavePreset} onLoadPreset={onLoadPreset} ImportDataButton={ImportDataButton} />
 
-        <div className="trend-bar" style={{ display: 'inline-flex', marginLeft: '1%',  border: '1px solid lightgray', padding:"5px"}}>
-                  <div  style={{ width: '180px' }}  >                    
-                    <FormControl fullWidth>
-                      <Tooltip title="show all records or only declining records" followCursor>
-                        <InputLabel id="trend-label">Trend</InputLabel>
-                      </Tooltip>
-                      <MuiSelect
-                        disabled = {props.isClassView} 
-                        labelId="trend-label"
-                        id="trend"
-                        value={props.trend}
-                        onChange={handleTrendChange}
-                        label="Trend"
-                      >
-                        {trendOptions.map((option) => (
-                          <MenuItem key={option.value} value={option.value}>
-                            {option.label}
-                          </MenuItem>
-                        ))}
-                      </MuiSelect>
-                    </FormControl>
-
-                  </div>
-                 
-                 <div style={{ width: '120px' }}>
-                  <DeclineThresholdSlider 
-                    trend = {props.trend}
-                    isDisabled = {props.trend === props.trendSet.all || props.isClassView}
-                    setThreshold={props.trend === props.trendSet.overall_decline? props.setDeclineSlope : props.setDiffThreshold}
-                    minThreshold={props.minDeclineThreshold}
-                    label = {trendToLabel[props.trend]|| "  "}
-                    filterWithTrendThreshold = {props.filterWithTrendThreshold}                    
-                  />
-                </div>
-          </div>
-
-        <div className ="show-lines"  style={{ display: 'inline-flex', marginLeft: '1%'}}>
-                <input 
-                    type="checkbox" 
-                    checked = {props.showLines}
-                    onChange={() => props.setShowLines(!props.showLines)} 
-                />
-
-                <Tooltip title="Line-connect records from identical individuals" followCursor>                
-                <label><br/>Show lines </label>
-                </Tooltip>
-        </div>
-
-        <div className = "show-class-view" style={{ display: 'inline-flex', marginLeft: '1%',  border: '1px solid lightgray'}}>
-                <input 
-                    type="checkbox" 
-                    checked = {props.isClassView}
-                    onChange={() => {props.setIsClassView(!props.isClassView); }}   
-                    style={{  marginLeft: '5%'}}
-                />
-                <Tooltip title="Switch between class-aggregation view and individual view" followCursor>
-                <label><br/>Class View </label>
-                </Tooltip>
-
-                {props.isClassView &&
-                  <div className='aggregate-buttons-row' 
-                    style={{ display: 'inline-flex', alignItems: 'center',marginRight:'20px', padding:'2px' }}>
-
-                      <FormControl  style={{fontSize:'12px'}}>
-                        <RadioGroup                          
-                          aria-labelledby="demo-controlled-radio-buttons-group"
-                          name="controlled-radio-buttons-group"
-                          value= {props.aggregateType}
-                          onChange={(event)=> props.setAggregateType(event.target.value)}
-                        >
-                          <FormControlLabel value="box" control={<Radio sx={{ '& .MuiSvgIcon-root': { fontSize: 12}}}/>}  label={<div style={{fontSize:12}}> Box </div>} />
-                          <FormControlLabel value="violin" control={<Radio sx={{ '& .MuiSvgIcon-root': { fontSize: 12}}}/>} label={<div style={{fontSize:12}}> Violin </div>}  />
-                          <FormControlLabel value="circle" control={<Radio sx={{ '& .MuiSvgIcon-root': { fontSize: 12}}}/>} label={<div style={{fontSize:12}}> Circle </div>} />
-                        </RadioGroup>
-                      </FormControl>
-
-                      <div  style={{ display: 'inline-block', marginLeft: '10%'}}>
-                          <input 
-                              type="checkbox" 
-                              checked={props.studentsChecked} 
-                              onChange={() => props.setStudentsChecked(!props.studentsChecked)}
-                          />
-                        <Tooltip title="Whether to show individual dots on the class view" followCursor>
-                          <label> <br />Present Individuals </label>
-                        </Tooltip>
-                      </div>
-
-                      <div  style={{ display: 'inline-block', marginLeft: '10%'}}>
-                          <input 
-                              type="checkbox" 
-                              checked={props.connectIndividual} 
-                              onChange={() => props.setConnectIndividual(!props.connectIndividual)}
-                          />
-                        <Tooltip title="Whether to show lines connecting individual dots on the class view" followCursor>
-                          <label>Connect individuals </label>
-                        </Tooltip>
-                      </div>
-                  
-                  </div>
-                }
-        </div>
-
-        <div className="preset-buttons-row" 
-         style={{display: 'inline-flex',border: '1px solid lightgray', padding:'5px', marginLeft: '1%' }}>
-
-              <button className="btn"
-                id="reset-btn"
-                onClick={() => props.handleResetToOnboarding()} // function to reset the state to the initial state
-              >
-               <Tooltip title="Reset to on-boarding view" followCursor>
-                Reset
-                </Tooltip>
-              </button>
-
-              <button className="btn"
-                id="reset-latest-btn"
-                onClick={() => props.handleResetToLatest()} // function to reset the state to the initial state
-              >
-                <Tooltip title="Reset to view of the last saved preset" followCursor>
-                Reset to latest saved
-                </Tooltip>
-              </button>
-
-              <button className="btn"                  
-                  id="save-preset-btn"
-                  //style={{ color: 'gray' }} 
-                  onClick={() => onSavePreset()} // function to save current state as a preset
-                >
-                  <Tooltip title="Save current filters, axis fields and view mode settings to a preset file" followCursor>
-                  Save Preset
-                  </Tooltip>
-                </button>
-
-              <button className="btn"
-                id="load-preset-btn"
-                onClick={() => onLoadPreset()} // function to load a saved preset
-              >
-                <Tooltip title="Load filters, axis fields and view mode settings from a preset" followCursor>
-                Load Preset
-                </Tooltip>
-              </button>
-
-              <ImportDataButton  handleFileUpload={props.handleFileUpload} />  
-
-
-        </div>
       </div>
+    </div>
+  );
+
+};
+
+function Axes({ props, x_options, y_options, colorOptions}) {
+  return (
+    <div className="axes" style={{ display: 'flex' }}>
+      <div className="field-pair"  >
+        <Tooltip title="Select variable on horizontal axis" followCursor>
+          <label htmlFor="x-field">X-field:</label>
+        </Tooltip>
+        <Select 
+          isDisabled = {props.isClassView} 
+          id="x-field"
+          value={{ value: props.xField, label: props.xField }}
+          options={x_options}
+          onChange={option => props.onXFieldChange(option.value)}
+        />
+      </div>       
+
+      <div className="field-pair" >
+        <Tooltip title="Select variable on vertical axis" followCursor>
+          <label htmlFor="y-field">Y-field:</label>
+        </Tooltip>
+        <Select
+          id="y-field"
+          value={{ value: props.yField, label: props.yField }}
+          options={y_options}
+          onChange={option => props.onYFieldChange(option.value)}
+        />
+      </div>
+
+      <div className="field-pair">
+      <Tooltip title="Select variable for color coded data" followCursor>
+        <label htmlFor="color-field">Color:</label>   
+      </Tooltip>   
+        <Select
+          isDisabled = {props.isClassView}
+          id="color-field"
+          value={{ value: props.colorField, label: props.colorField }}
+          options={colorOptions}
+          onChange={option => props.onColorFieldChange(option.value)}
+        />
+      </div>
+    </div>
+  );
+}
+
+function TrendBar({props, trendOptions, handleTrendChange, trendToLabel}) {
+
+  return(
+    <div className="trend-bar" style={{ display: 'inline-flex', marginLeft: '1%',  border: '1px solid lightgray', padding:"5px"}}>
+      <div  style={{ width: '180px' }}  >                    
+        <FormControl fullWidth>
+          <Tooltip title="show all records or only declining records" followCursor>
+            <InputLabel id="trend-label">Trend</InputLabel>
+          </Tooltip>
+          <MuiSelect
+            disabled = {props.isClassView} 
+            labelId="trend-label"
+            id="trend"
+            value={props.trend}
+            onChange={handleTrendChange}
+            label="Trend"
+          >
+            {trendOptions.map((option) => (
+              <MenuItem key={option.value} value={option.value}>
+                {option.label}
+              </MenuItem>
+            ))}
+          </MuiSelect>
+        </FormControl>
+
+      </div>
+
+      <div style={{ width: '120px' }}>
+      <DeclineThresholdSlider 
+        trend = {props.trend}
+        isDisabled = {props.trend === props.trendSet.all || props.isClassView}
+        setThreshold={props.trend === props.trendSet.overall_decline? props.setDeclineSlope : props.setDiffThreshold}
+        minThreshold={props.minDeclineThreshold}
+        label = {trendToLabel[props.trend]|| "  "}
+        filterWithTrendThreshold = {props.filterWithTrendThreshold}                    
+      />
+      </div>
+    </div>
+  );
+
+}
+
+function ShowLinesToggle({props}) {
+
+  return (
+    <div className ="show-lines"  style={{ display: 'inline-flex', marginLeft: '1%'}}>
+      <input 
+          type="checkbox" 
+          checked = {props.showLines}
+          onChange={() => props.setShowLines(!props.showLines)} 
+      />
+
+      <Tooltip title="Line-connect records from identical individuals" followCursor>                
+      <label><br/>Show lines </label>
+      </Tooltip>
+  </div>
+  );
+
+}
+
+function ClassViewBar({props}) {
+
+  return (
+    <div className = "show-class-view" style={{ display: 'inline-flex', marginLeft: '1%',  border: '1px solid lightgray'}}>
+      <input 
+          type="checkbox" 
+          checked = {props.isClassView}
+          onChange={() => {props.setIsClassView(!props.isClassView); }}   
+          style={{  marginLeft: '5%'}}
+      />
+
+      <Tooltip title="Switch between class-aggregation view and individual view" followCursor>
+      <label><br/>Class View </label>
+      </Tooltip>
+
+      {props.isClassView &&
+        <div className='aggregate-buttons-row' 
+          style={{ display: 'inline-flex', alignItems: 'center',marginRight:'20px', padding:'2px' }}>
+
+            <FormControl  style={{fontSize:'12px'}}>
+              <RadioGroup                          
+                aria-labelledby="demo-controlled-radio-buttons-group"
+                name="controlled-radio-buttons-group"
+                value= {props.aggregateType}
+                onChange={(event)=> props.setAggregateType(event.target.value)}
+              >
+                <FormControlLabel value="box" control={<Radio sx={{ '& .MuiSvgIcon-root': { fontSize: 12}}}/>}  label={<div style={{fontSize:12}}> Box </div>} />
+                <FormControlLabel value="violin" control={<Radio sx={{ '& .MuiSvgIcon-root': { fontSize: 12}}}/>} label={<div style={{fontSize:12}}> Violin </div>}  />
+                <FormControlLabel value="circle" control={<Radio sx={{ '& .MuiSvgIcon-root': { fontSize: 12}}}/>} label={<div style={{fontSize:12}}> Circle </div>} />
+              </RadioGroup>
+            </FormControl>
+
+            <div  style={{ display: 'inline-block', marginLeft: '10%'}}>
+                <input 
+                    type="checkbox" 
+                    checked={props.studentsChecked} 
+                    onChange={() => props.setStudentsChecked(!props.studentsChecked)}
+                />
+              <Tooltip title="Whether to show individual dots on the class view" followCursor>
+                <label> <br />Present Individuals </label>
+              </Tooltip>
+            </div>
+
+            <div  style={{ display: 'inline-block', marginLeft: '10%'}}>
+                <input 
+                    type="checkbox" 
+                    checked={props.connectIndividual} 
+                    onChange={() => props.setConnectIndividual(!props.connectIndividual)}
+                />
+              <Tooltip title="Whether to show lines connecting individual dots on the class view" followCursor>
+                <label>Connect individuals </label>
+              </Tooltip>
+            </div>
+        
+        </div>
+      }
     </div>
 
   );
-};
+}
 
+function PresetBar({props, onSavePreset, onLoadPreset, ImportDataButton}) {
+
+  return (
+
+    <div className="preset-buttons-row" 
+      style={{display: 'inline-flex',border: '1px solid lightgray', padding:'5px', marginLeft: '1%' }}>
+
+          <button className="btn"
+            id="reset-btn"
+            onClick={() => props.handleResetToOnboarding()} // function to reset the state to the initial state
+          >
+            <Tooltip title="Reset to on-boarding view" followCursor>
+            Reset
+            </Tooltip>
+          </button>
+
+          <button className="btn"
+            id="reset-latest-btn"
+            onClick={() => props.handleResetToLatest()} // function to reset the state to the initial state
+          >
+            <Tooltip title="Reset to view of the last saved preset" followCursor>
+            Reset to latest saved
+            </Tooltip>
+          </button>
+
+          <button className="btn"                  
+              id="save-preset-btn"
+              //style={{ color: 'gray' }} 
+              onClick={() => onSavePreset()} // function to save current state as a preset
+            >
+              <Tooltip title="Save current filters, axis fields and view mode settings to a preset file" followCursor>
+              Save Preset
+              </Tooltip>
+            </button>
+
+          <button className="btn"
+            id="load-preset-btn"
+            onClick={() => onLoadPreset()} // function to load a saved preset
+          >
+            <Tooltip title="Load filters, axis fields and view mode settings from a preset" followCursor>
+            Load Preset
+            </Tooltip>
+          </button>
+
+          <ImportDataButton  handleFileUpload={props.handleFileUpload} />  
+
+
+    </div>
+
+  );
+}
 
 function DeclineThresholdSlider({ trend, setThreshold, isDisabled, minThreshold, label, filterWithTrendThreshold }) {
   const max = 0;
