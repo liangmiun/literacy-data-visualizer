cce097bd80398492d7cebd14af8527d4cfc601ec
Filip Berendt <filipberendt11@gmail.com>
2024-06-24 13:45:01 +0200

    Update About.js

--- src/components/screens/About.js
+++ src/components/screens/About.js
@@ -4,7 +4,13 @@ const About = () => {
 
     return (
         <>
-            <div>About Page</div>
+            <div>Denna visualisering har utvecklats av InfraVis, den nationella forskningsinfrastrukteren för visualisering av data.</div><br></br>
+            <div>Användrna är Åke Grönlund från Handelshögskolan vid Örebro Universitet, Gustaf Öqvist Seimyr från Karolinska Institutet och CSO för lexplore, och Olga Viberg från Kungliga Tekniska Högskolan.</div><br></br>
+            <div>Från InfraVis håll har följande personerer varit involverade:</div><br></br>
+            <div><b>Projektansvarig</b>: Mario Romero (KTH)</div>
+            <div><b>Projektledare</b>: Filip Berendt (KTH)</div>
+            <div><b>Utvecklare</b>: Liang Zhou (MIUN), Sergey Ignatenko (LiU), Filip Berendt (KTH)</div><br></br>
+            <div>Läs mer om InfraVis <a href="https://infravis.se">här</a>.</div>
         </>
     );
 };
